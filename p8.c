66c367823dfb07017aa190a4ac18ecb947fd6591
Tejas M <97739480+playgms@users.noreply.github.com>
2024-12-19 07:21:26 +0530

    Update p8.c

--- p8.c
+++ p8.c
@@ -1,63 +1,32 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <mpi.h>
 
-#define SIZE 16 // Total number of processes
-#define UP 0    // Direction indices
-#define DOWN 1
-#define LEFT 2
-#define RIGHT 3
-
 int main(int argc, char* argv[]) {
-    int numtasks, rank, source, dest, outbuf, i, tag = 1;
-    int inbuf[4] = { MPI_PROC_NULL, MPI_PROC_NULL, MPI_PROC_NULL, MPI_PROC_NULL };
-    int nbrs[4], dims[2] = { 4, 4 }, periods[2] = { 0, 0 }, reorder = 0, coords[2];
-    MPI_Request reqs[8];
-    MPI_Status stats[8];
+    int numtasks, rank;
+    int dims[2], periods[2] = {0, 0}, coords[2];
     MPI_Comm cartcomm;
 
     MPI_Init(&argc, &argv);                   // Initialize MPI
     MPI_Comm_size(MPI_COMM_WORLD, &numtasks); // Get the number of processes
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);     // Get the rank of this process
 
-    if (numtasks == SIZE) {
-        // Create a 2D Cartesian topology
-        MPI_Cart_create(MPI_COMM_WORLD, 2, dims, periods, reorder, &cartcomm);
+    // Define a 2D Cartesian grid
+    dims[0] = 2;             // Rows
+    dims[1] = numtasks / 2;  // Columns (adjust to the number of tasks)
 
-        // Get the rank and coordinates in the Cartesian topology
-        MPI_Comm_rank(cartcomm, &rank);
-        MPI_Cart_coords(cartcomm, rank, 2, coords);
+    // Create the Cartesian topology
+    MPI_Cart_create(MPI_COMM_WORLD, 2, dims, periods, 0, &cartcomm);
 
-        // Find the neighbors in all four directions
-        MPI_Cart_shift(cartcomm, 0, 1, &nbrs[UP], &nbrs[DOWN]);
-        MPI_Cart_shift(cartcomm, 1, 1, &nbrs[LEFT], &nbrs[RIGHT]);
+    // Get the Cartesian coordinates of this process
+    MPI_Cart_coords(cartcomm, rank, 2, coords);
 
-        // Print the rank, coordinates, and neighbors
-        printf("rank= %d coords= %d %d neighbors(u,d,l,r)= %d %d %d %d\n",
-               rank, coords[0], coords[1], nbrs[UP], nbrs[DOWN], nbrs[LEFT], nbrs[RIGHT]);
-
-        // Send and receive data to/from neighbors
-        outbuf = rank;
-        for (i = 0; i < 4; i++) {
-            dest = nbrs[i];
-            source = nbrs[i];
-            MPI_Isend(&outbuf, 1, MPI_INT, dest, tag, cartcomm, &reqs[i]);
-            MPI_Irecv(&inbuf[i], 1, MPI_INT, source, tag, cartcomm, &reqs[i + 4]);
-        }
-
-        // Wait for all non-blocking operations to complete
-        MPI_Waitall(8, reqs, stats);
-
-        // Print the data received from neighbors
-        printf("rank= %d inbuf(u,d,l,r)= %d %d %d %d\n",
-               rank, inbuf[UP], inbuf[DOWN], inbuf[LEFT], inbuf[RIGHT]);
-    } else {
-        // Ensure the program is run with the correct number of processes
-        printf("Must specify %d tasks. Terminating.\n", SIZE);
-    }
+    // Print the rank and coordinates
+    printf("Rank %d: Coordinates = (%d, %d)\n", rank, coords[0], coords[1]);
 
     MPI_Finalize(); // Finalize MPI
     return 0;
 }
+
 
 
 //mpicc -o cartesian_topology cartesian_topology.c
