4d1a9bd9894a4a6c03551931b823d5ebbc567813
Tejas M <97739480+playgms@users.noreply.github.com>
2024-12-19 07:38:59 +0530

    Update p9.c

--- p9.c
+++ p9.c
@@ -3,23 +3,17 @@
 
 int main(int argc, char* argv[]) {
     int rank, size, next, prev, inmsg;
-    MPI_Request reqs[2];
-    MPI_Status stats[2];
 
-    MPI_Init(&argc, &argv);                   // Initialize MPI
-    MPI_Comm_size(MPI_COMM_WORLD, &size);     // Get the number of processes
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);     // Get the rank of this process
+    MPI_Init(&argc, &argv);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
     // Determine neighbors
-    next = (rank + 1) % size;                 // Next process in ring
-    prev = (rank - 1 + size) % size;          // Previous process in ring
+    next = (rank + 1) % size;
+    prev = (rank - 1 + size) % size;
 
-    // Blocking Send/Receive
-    if (rank == 0) {
-        printf("Blocking Send/Receive:\n");
-    }
+    // Blocking Communication
     MPI_Barrier(MPI_COMM_WORLD); // Synchronize before starting
-
     if (rank % 2 == 0) {
         MPI_Send(&rank, 1, MPI_INT, next, 0, MPI_COMM_WORLD);
         MPI_Recv(&inmsg, 1, MPI_INT, prev, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
@@ -28,24 +22,29 @@ int main(int argc, char* argv[]) {
         MPI_Send(&rank, 1, MPI_INT, next, 0, MPI_COMM_WORLD);
     }
 
-    printf("Rank %d received %d (Blocking)\n", rank, inmsg);
-
-    // Non-blocking Send/Receive
-    if (rank == 0) {
-        printf("\nNon-blocking Send/Receive:\n");
+    // Ensure ordered output for Blocking
+    for (int i = 0; i < size; i++) {
+        MPI_Barrier(MPI_COMM_WORLD);
+        if (rank == i) {
+            printf("Rank %d received %d (Blocking)\n", rank, inmsg);
+        }
     }
-    MPI_Barrier(MPI_COMM_WORLD); // Synchronize before starting
 
+    // Non-blocking Communication
+    MPI_Request reqs[2];
+    MPI_Barrier(MPI_COMM_WORLD); // Synchronize before starting
     MPI_Irecv(&inmsg, 1, MPI_INT, prev, 0, MPI_COMM_WORLD, &reqs[0]);
     MPI_Isend(&rank, 1, MPI_INT, next, 0, MPI_COMM_WORLD, &reqs[1]);
+    MPI_Waitall(2, reqs, MPI_STATUSES_IGNORE);
 
-    MPI_Waitall(2, reqs, stats); // Wait for both non-blocking operations to complete
+    // Ensure ordered output for Non-blocking
+    for (int i = 0; i < size; i++) {
+        MPI_Barrier(MPI_COMM_WORLD);
+        if (rank == i) {
+            printf("Rank %d received %d (Non-blocking)\n", rank, inmsg);
+        }
+    }
 
-    printf("Rank %d received %d (Non-blocking)\n", rank, inmsg);
-
-    MPI_Finalize(); // Finalize MPI
+    MPI_Finalize();
     return 0;
 }
-
-//mpicc p9.c
-//mpirun -np 4 ./a.out
